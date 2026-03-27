28d2527dd56941bc5dc763177a2db83eb0fedfdf
mohanrajs <mohanraj.srinivasan@dreamstechnologies.com>
2026-03-27 09:18:28 +0530

    Fix vite config for build

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/agricore/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-        }
-      }
-    }
-  }
 })
